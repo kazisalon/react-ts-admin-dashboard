0927ae6f0679d52643251035ef12661214ead579
kazisalon <kajisalon@github.com>
2025-01-03 13:17:45 +0545

    Updated Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -58,18 +58,15 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   return (
     <aside
       ref={sidebar}
-      className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
-        sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+      className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-[#003c44] duration-300 ease-linear dark:bg-[#003c44] lg:static lg:translate-x-0 ${
+    sidebarOpen ? 'translate-x-0' : '-translate-x-full'
       }`}
     >
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
-        {/* <NavLink to="/">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Caduceus.svg/1200px-Caduceus.svg.png"
-            alt="Caduceus Medical Symbol"
-          />
-        </NavLink> */}
+        <NavLink to="/">
+        <img src="/src/components/Sidebar/Haleshi.png" alt="Haleshi" />
+        </NavLink>
 
         <button
           ref={trigger}
